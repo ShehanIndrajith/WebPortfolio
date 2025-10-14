282922ecb76453fbe7ea92f0bf7650085c9b5460
Shehan Indrajith <sbsindrajith2002@gmail.com>
2025-10-14 20:04:56 +0530

    adjust Backgound height

--- src/App.jsx
+++ src/App.jsx
@@ -118,7 +118,7 @@ function App() {
             {/* Background */}
             <div className="fixed top-0 -z-50 h-full w-full">
               <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" >
-                <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+                
   <Particles
     particleColors={['#ffffff', '#ffffff']}
     particleCount={200}
@@ -129,7 +129,6 @@ function App() {
     alphaParticles={false}
     disableRotation={false}
   />
-</div>
               </div>
               
             </div>
